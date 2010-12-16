b347290d0949e47552fd6cda0e157c6c9029439c
Vladimir Yatsevsky <vv.yatsevsky@gmail.com>
2010-12-16 23:02:25 +0200

    Added skeleton project

---
+++ src/ua/biz/shaman/crossword/CrosswordDemoActivity.java
@@ -0,0 +1,13 @@
+package ua.biz.shaman.crossword;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CrosswordDemoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
